e2526484b1f0abd119b25433b3bab32d1a2d5afc
Ivan Chavero <ichavero@redhat.com>
2019-02-06 15:52:12 -0700

    Add file check

--- show.sh
+++ show.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Process DocBook file and converts it to PDF
+# Iván Chavero <ichavero@chavero.com.mx>
+
+
+if [ ! -f $1.xml ]; then
+    echo "$1.xml File not found!"
+    exit
+fi
+
 echo "Processing $1.xml"
 echo "Running XML Lint"
 xmllint --xinclude $1.xml --output $1-lint.xml
