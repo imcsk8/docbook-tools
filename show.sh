7de7aad1e2d7465029cf068aa97d7648e2957ad1
Ivan Chavero <ichavero@redhat.com>
2019-02-06 15:49:50 -0700

    Add XML Lint processing

--- show.sh
+++ show.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "Processing $1.xml"
-/usr/bin/pandoc --from docbook --to latex --output $1.pdf $1.xml
-evince $1.pdf
+echo "Running XML Lint"
+xmllint --xinclude $1.xml --output $1-lint.xml
+echo "Converting to PDF"
+/usr/bin/pandoc --from docbook --to latex --output $1.pdf $1-lint.xml
+echo "Running evince viewer"
+/usr/bin/evince $1.pdf
